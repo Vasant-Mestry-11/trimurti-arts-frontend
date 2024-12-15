07e3b51b134966181392ea4e301dd579f96f6ec1
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-15 21:21:04 +0530

    :zap: :art: localhost and prod url added

--- src/components/routes/Admin.jsx
+++ src/components/routes/Admin.jsx
@@ -3,17 +3,18 @@ import useAuth from "../../hooks/useAuth";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../Spinner";
+import useGetURL from "../../hooks/useGetURL";
 
 export default function AdminPrivateRoute() {
   const [ok, setOk] = useState(false);
 
   const [auth] = useAuth();
 
+  const url = useGetURL();
+
   useEffect(() => {
     const authCheck = async () => {
-      const res = await axios.get(
-        "http://localhost:8080/api/v1/auth/admin-auth"
-      );
+      const res = await axios.get(`${url}/auth/admin-auth`);
       if (res?.data?.ok) {
         setOk(true);
       } else {
@@ -24,6 +25,6 @@ export default function AdminPrivateRoute() {
     if (auth?.token) {
       authCheck();
     }
-  }, [ok, auth?.token]);
+  }, [ok, auth?.token, url]);
   return ok ? <Outlet /> : <Spinner path="/" />;
 }
--- src/components/routes/Private.jsx
+++ src/components/routes/Private.jsx
@@ -3,17 +3,18 @@ import useAuth from "../../hooks/useAuth";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../Spinner";
+import useGetURL from "../../hooks/useGetURL";
 
 export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
 
   const [auth] = useAuth();
 
+  const url = useGetURL();
+
   useEffect(() => {
     const authCheck = async () => {
-      const res = await axios.get(
-        "http://localhost:8080/api/v1/auth/user-auth"
-      );
+      const res = await axios.get(`${url}/auth/user-auth`);
       if (res?.data?.ok) {
         setOk(true);
       } else {
