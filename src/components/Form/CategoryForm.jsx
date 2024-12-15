4e56df0a8fbddb293b966871407540777330e2c5
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-15 13:45:03 +0530

    :zap: :art: Category Create form added

---
+++ src/components/Form/CategoryForm.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+
+const CategoryForm = ({ onSubmit, value, onChange, name }) => {
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter new category"
+            value={value}
+            onChange={onChange}
+            name={name}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CategoryForm;
+
+CategoryForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+};
--- src/pages/admin/CreateCategory.jsx
+++ src/pages/admin/CreateCategory.jsx
@@ -3,9 +3,13 @@ import toast from "react-hot-toast";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "../../components/Layout/Layout";
 import axios from "axios";
+import CategoryForm from "../../components/Form/CategoryForm";
 
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
+  const [formData, setFormData] = useState({});
+
+  const { categoryName } = formData;
 
   const getAllCategories = async () => {
     try {
@@ -16,11 +20,37 @@ const CreateCategory = () => {
       if (success) {
         setCategories(categories);
       }
-      // return data;
     } catch (error) {
       console.log(error);
       toast.error("Failed to fetch categories");
     }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8080/api/v1/category/create-category",
+        { name: categoryName }
+      );
+      const { success } = data;
+      if (success) {
+        toast.success("Category created successfully");
+        getAllCategories();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to submit category");
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
 
   useEffect(() => {
@@ -36,6 +66,14 @@ const CreateCategory = () => {
           <div className="col-md-9">
             <h1>Manage category</h1>
 
+            <div className="p-3 w-50">
+              <CategoryForm
+                value={categoryName}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+                name="categoryName"
+              />
+            </div>
             <div className="w-75">
               <table className="table">
                 <thead>
