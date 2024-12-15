97f8006958ea3b310886d659b5320dd4cb60956d
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-15 21:10:22 +0530

    :zap: :art: localhost and prod url added

--- src/hooks/useGetURL.jsx
+++ src/hooks/useGetURL.jsx
@@ -1,7 +1,7 @@
 import { LOCAL_URLS, PROD_URLS } from "../config/urls.config";
 
 const useGetURL = () => {
-  const url = import.meta.PROD ? PROD_URLS.url : LOCAL_URLS.url;
+  const url = import.meta.env.PROD ? PROD_URLS.url : LOCAL_URLS.url;
   return url;
 };
 
