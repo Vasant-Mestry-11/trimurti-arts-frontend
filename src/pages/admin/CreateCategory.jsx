85dc7322a90a50db6e7ca6c3120b3387fb553eea
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-15 13:18:48 +0530

    :zap: :art: :sparkles: manage categories

--- src/pages/admin/CreateCategory.jsx
+++ src/pages/admin/CreateCategory.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "../../components/Layout/Layout";
+import axios from "axios";
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([]);
+
+  const getAllCategories = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/category/get-all-categories"
+      );
+      const { categories, success } = data;
+      if (success) {
+        setCategories(categories);
+      }
+      // return data;
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to fetch categories");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategories();
+  }, []);
   return (
     <Layout title="Dashboard - Create Category">
       <div className="container-fluid m-3 p-3">
@@ -10,7 +34,28 @@ const CreateCategory = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <h1>Create category</h1>
+            <h1>Manage category</h1>
+
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map(({ _id, name }) => (
+                    <tr key={_id}>
+                      <td>{name}</td>
+                      <td>
+                        <button className="btn btn-primary">Edit</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
